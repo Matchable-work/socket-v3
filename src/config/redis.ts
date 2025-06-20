ec3e17165bf53f55162d92bd52c0d48feb2eb3cf
Mahbubul Alam <mahbubulalam5676@gmail.com>
2025-06-20 14:31:03 +0600

    fix: redis

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -5,6 +5,7 @@ export const createRedisClient = () =>
         host: process.env.REDIS_HOST,
         port: Number(process.env.REDIS_PORT),
         password: process.env.REDIS_PASSWORD || undefined,
+        maxRetriesPerRequest: null, // ✅ REQUIRED by BullMQ
     });
 
 // ✅ Add this for compatibility
